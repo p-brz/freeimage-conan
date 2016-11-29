31dfbcf7a0c39cea89787e66dd7b0ebdc0c72b7b
Paulo Brizolara <souzabrizolara@gmail.com>
2016-11-28 21:56:16 -0300

    Adapting recipe for compiling using Android ndk-toolchain.

--- conanfile.py
+++ conanfile.py
@@ -20,8 +20,7 @@ class FreeImageConan(ConanFile):
         "use_cxx_wrapper=True"
     )
 
-    patches = ("Makefile.gnu", "Makefile.fip")
-    exports = patches
+    exports = ("patches/*")
 
     #Downloading from sourceforge
     REPO = "http://downloads.sourceforge.net/project/freeimage/"
@@ -38,25 +37,24 @@ class FreeImageConan(ConanFile):
         tools.unzip(zip_name)
         os.unlink(zip_name)
 
-        self.output.info(os.listdir("."))
-
-        for p in self.patches:
-            copyfile(path.join(".", p), path.join(self.UNZIPPED_DIR, p))
-
+        self.apply_patches()
+            
     def build(self):
         env = ConfigureEnvironment(self)
 
         make_env = self.make_env()
-        make_cmd = "%s %s make" % (env.command_line, make_env) 
+        env_line = "%s %s " % (env.command_line, make_env)
         
-        self.run(make_cmd             , cwd=self.UNZIPPED_DIR)
-        self.run(make_cmd + " install", cwd=self.UNZIPPED_DIR)
-
-        if self.options.use_cxx_wrapper:
-            make_cxx_cmd = "%s %s make -f Makefile.fip" % (env.command_line, make_env)
+        self.make_and_install(env_line)
             
-            self.run(make_cxx_cmd               , cwd=self.UNZIPPED_DIR)
-            self.run(make_cxx_cmd + " install"  , cwd=self.UNZIPPED_DIR)
+        if self.options.use_cxx_wrapper:
+            self.make_and_install(env_line, "-f Makefile.fip")
+               
+    def make_and_install(self, env_line, options=""):
+        make_cmd = "%s make %s" % (env_line, options)
+     
+        self.print_and_run(make_cmd               , cwd=self.UNZIPPED_DIR)
+        self.print_and_run(make_cmd + " install"  , cwd=self.UNZIPPED_DIR)
 
     def package(self):
         pass
@@ -67,6 +65,14 @@ class FreeImageConan(ConanFile):
         if self.options.use_cxx_wrapper:
             self.cpp_info.libs.append("freeimageplus")
 
+    ################################ Helpers ######################################
+
+    def print_and_run(self, cmd, **kw):
+        cwd_ = "[%s] " % kw.get('cwd') if kw.has_key('cwd') else ''
+        
+        self.output.info(cwd_ + str(cmd))
+        self.run(cmd, **kw)
+        
     def make_env(self):
         env = []
         
@@ -76,6 +82,9 @@ class FreeImageConan(ConanFile):
         if not hasattr(self, 'package_folder'):
             self.package_folder = "dist"
         
+        if self.settings.os == "Android":
+            env.append("NO_SWAB=1")
+        
         env.append("DESTDIR=" + self.package_folder)
         env.append("INCDIR=" + path.join(self.package_folder, "include"))
         env.append("INSTALLDIR=" + path.join(self.package_folder, "lib"))
@@ -84,3 +93,49 @@ class FreeImageConan(ConanFile):
 
     def rename_file(self, dir, filename, newname):
         os.rename(path.join(dir, filename), path.join(dir, newname))
+
+
+    def apply_patches(self):
+        self.output.info("Applying patches")
+        
+        #Copy "patch" files
+        self.copy_tree("patches", self.UNZIPPED_DIR)
+
+        self.patch_android_swab_issues()
+        self.patch_android_neon_issues()
+        
+    def patch_android_swab_issues(self):
+        librawlite = path.join(self.UNZIPPED_DIR, "Source", "LibRawLite")
+        missing_swab_files = [
+            path.join(librawlite, "dcraw", "dcraw.c"),
+            path.join(librawlite, "internal", "defines.h")
+        ]
+        replaced_include = '\n'.join(('#include <unistd.h>', '#include "swab.h"'))
+        
+        for f in missing_swab_files:
+            self.output.info("patching file '%s'" % f)
+            tools.replace_in_file(f, "#include <unistd.h>", replaced_include)
+
+    def patch_android_neon_issues(self):
+        # avoid using neon
+        libwebp_src = path.join(self.UNZIPPED_DIR, "Source", "LibWebP", "src")
+        rm_neon_files = [   path.join(libwebp_src, "dsp", "dsp.h") ]
+        for f in rm_neon_files:
+            self.output.info("patching file '%s'" % f)
+            tools.replace_in_file(f, "#define WEBP_ANDROID_NEON", "")
+
+    def copy_tree(self, src_root, dst_root):
+#        for p in self.patches:
+#        for p in os.listdir('patches'):
+        for root, dirs, files in os.walk(src_root):
+            for d in dirs:
+                dst_dir = path.join(dst_root, d)
+                if not path.exists(dst_dir):
+                    os.mkdir(dst_dir)
+                    
+                self.copy_tree(path.join(root, d), dst_dir)
+            
+            for f in files:
+                copyfile(path.join(root, f), path.join(dst_root, f))
+
+            break
